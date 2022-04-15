619d648443705c511b3902fb8e59ebfd5972d50f
Sebastian Salazar <seb.salazar.coding@gmail.com>
2022-04-15 18:48:50 -0400

    add letters' colourization upon tries

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 from getpass import getpass
-from typing import List
+from typing import List, Tuple
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ENDC = '\033[0m'
+
+
+def replace_str_at_position(string: str, new_character: str, position: int):
+    string = string[:position] + new_character + string[position+1:]
+    return string
 
 
 def print_grid_and_words(area: int, unit: int, words_list: List[str]):
     """area (a): a x a grid. unit: the number of | of each box side"""
+    global result_index_and_letters, day_word_list_with_indexes, day_word_list, user_word_list
 
+    partial_matches  = set(day_word_list) & set(user_word_list)
+    exact_matches: Tuple[int, str] = set(day_word_list_with_indexes) & set(result_index_and_letters)
+
+    # Colour the letters
+    word = list(words_list[-1])
+    for index, letter in enumerate(word):
+        if letter in partial_matches:
+            word[index] = bcolors.WARNING + letter + bcolors.ENDC
+
+    for index, found_letter in exact_matches:
+        word[index] = bcolors.OKGREEN + found_letter + bcolors.ENDC
+
+    words_list[-1] = word
     rows_to_print = [("|   ") * unit * area] * area
     for index, word in enumerate(words_list):
         row_to_print = ""
         for letter in word:
-            row_to_print += "|"*unit + f" {letter} "
+            row_to_print += "|"*unit + f" {bcolors.BOLD + letter + bcolors.ENDC} "
 
         rows_to_print[index] = row_to_print
 
@@ -20,11 +51,12 @@ def print_grid_and_words(area: int, unit: int, words_list: List[str]):
 
 
 def input_day_word():
-    global day_word, day_word_list
+    global n_letters_allowed, day_word, day_word_list, day_word_list_with_indexes
 
-    day_word = str(getpass("Day Word: ")).upper()
+    day_word = str(getpass("Day Word: " + "*" * n_letters_allowed)).upper()
     day_word_list = list(day_word)
     run_validations()
+    day_word_list_with_indexes = [(index, letter) for index, letter in enumerate(day_word_list)]
     input_and_set()
 
 
@@ -49,36 +81,32 @@ def run_validations():
 
 
 def compare_and_yield_results():
-    global counter, n_letters_allowed, day_word, day_word_list, user_word_list, result_index_and_letters, words_try_list
+    global counter, n_letters_allowed, day_word, day_word_list, day_word_list_with_indexes, user_word_list, result_index_and_letters, words_try_list
 
     user_word = "".join(user_word_list)
     counter += 1
 
-    if day_word == user_word:
-        print(f"You won! Congratulations!")
-        exit()
-
     words_try_list.append(user_word)
 
-    day_word_index_and_letters = list(enumerate(day_word_list))
-    for letter in user_word_list:
+    result_index_and_letters = []
+    for index, letter in enumerate(user_word_list):
         results = [
             (index, element)
-            for index, element in day_word_index_and_letters if element == letter
+            for _, element in day_word_list_with_indexes if element == letter
         ]
         result_index_and_letters += results
 
     result_index_and_letters = list(set(result_index_and_letters))
-    # print(result_index_and_letters)
     partial_result_list = ["_ "]*n_letters_allowed
     for index, element in result_index_and_letters:
         partial_result_list[index] = element
 
-    partial_result_string = "".join(partial_result_list)
-    
-    # print(partial_result_string)
-
     print_grid_and_words(n_letters_allowed, 1, words_try_list)
+
+    if day_word == user_word:
+        print(f"You won! Congratulations! :D")
+        exit()
+
     if counter == 5:
         print(f"You lost :(\nThe actual word was {day_word}")
         exit()
@@ -93,6 +121,7 @@ if __name__ == "__main__":
     result_index_and_letters = []
     user_word_list = []
     words_try_list = []
+    day_word_list_with_indexes = []
     counter = 0
 
     input_day_word()
